274ff4f26bcb95a6710e38500c3790a80e3eec6b
Jeancarlo Zúñiga <qzuniga05@gmail.com>
2025-10-17 11:39:42 -0600

    Ajustes para Halloween

--- src/Web/Pages/Basket/Success.cshtml.cs
+++ src/Web/Pages/Basket/Success.cshtml.cs
@@ -5,14 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.FeatureManagement;
 
 namespace Microsoft.eShopWeb.Web.Pages.Basket;
 
 [Authorize]
 public class SuccessModel : PageModel
 {
-    public void OnGet()
-    {
+    private readonly IFeatureManager _featureManager;
+    public string SpecialMessage { get; private set; } = string.Empty;
 
+    public SuccessModel(IFeatureManager featureManager)
+    {
+        _featureManager = featureManager;
+    }
+
+    public async Task OnGet()
+    {
+        if(await _featureManager.IsEnabledAsync("EnableHalloween"))
+        {
+            SpecialMessage = "🎃 ¡Gracias por tu compra y feliz Halloween! 👻";
+        }
     }
 }
